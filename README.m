bda193c3086487321bfd59a1312496c291be36a7
James Ross <jamesross@macbookpro.lan>
2025-11-09 22:20:55 -0800

    final submission 11.9

---
+++ README.m
@@ -0,0 +1,27 @@
+
+#TITLE OF PROJECT:
+#Crocodile Observations
+
+#USERS: James Ross and Berenice Gonzalez
+
+#SUMMARY:
+#For our project, we worked with a dataset containing information on crocodile genera, age classes, and observed lengths. We organized the age groups, which were the Hatchling, Juvenile, Subadult, Adult age groups and added a loop with three elements representing the three genera in the dataset. Inside the loop, we used a conditional statement to print the average observed length for each type of crocodile, categorizing them as large, medium, or small based on their average size. We then created a ggplot2 heatmap to visualize the observed lengths across different age classes and genera, using a color gradient to highlight size differences. This allowed us to both analyze and visually compare crocodile growth patterns among species.
+
+#OVERVIEW:
+#In our project, we ended up using a for loop and conditional if statement to portray the observed lengths of crocodiles based on age and species. 
+
+#INSTRUCTIONS:
+#In order to run the data smoothly, use the github repository link and run the entire RMD file at the start. This will load in the libraries we used as well as allow users to view the graphs created. The first R code shows an original graph of the data which we used as a reference before including the conditional and loop aspects in the project 2 draft section. 
+
+
+
+
+
+
+
+
+
+
+
+
+
